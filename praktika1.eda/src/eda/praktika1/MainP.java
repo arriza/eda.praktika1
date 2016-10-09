5ab47a1e42f5547727c00dadf97ace97fe1bf677
Leire Viseras <leireviseras@gmail.com>
2016-10-09 14:18:14 +0200

    behar ez ditugun klaseak ezabatzen...

--- praktika1.eda/src/eda/praktika1/MainP.java
+++ praktika1.eda/src/eda/praktika1/MainP.java
@@ -47,6 +47,9 @@ public class MainP {
 		
 		//aktoreen zerrenda fitxategi batean gorde
 		Writer.getWriter().idatziAktoreenFitx();
+		
+		//aktoreen zerrenda ordenatua lortu --> Abizena, Izena
+		
 	}
 
 }
