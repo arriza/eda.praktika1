b20d84faa312730f9216d9d1ba5222e68f864fa9
Leire Viseras <leireviseras@gmail.com>
2016-10-26 17:22:24 +0200

    emandako klaseak

---
+++ praktika2.eda/src/praktika2/eda/Node.java
@@ -0,0 +1,15 @@
+package praktika2.eda;
+
+	public class Node<T> {
+		public T data; 			// dato del nodo
+		public Node<T> next; 	// puntero al siguiente nodo de la lista
+		// -------------------------------------------------------------
+
+		public Node(T dd) 		// constructor
+		{
+			data = dd;
+			next = null;
+		}
+	}
+
+
---
+++ praktika2.eda/src/praktika2/eda/OrderedCircularLinkedList.java
@@ -0,0 +1,13 @@
+package praktika2.eda;
+
+public class OrderedCircularLinkedList<T> extends CircularLinkedList<T> implements OrderedListADT<T> {
+	
+	public void add(T elem){
+		// KODEA OSATU ETA KOSTUA KALKULATU
+
+
+
+	}
+
+
+}
