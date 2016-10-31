37b7a7171954ceb839fc2882179e17157a4ff691
Leire Viseras <leireviseras@gmail.com>
2016-10-31 20:25:15 +0100

    UnorderedCirlularLinkedList

--- praktika2.eda/src/praktika2/eda/UnorderedCircularLinkedList.java
+++ praktika2.eda/src/praktika2/eda/UnorderedCircularLinkedList.java
@@ -1,21 +1,45 @@
 package praktika2.eda;
 
 public class UnorderedCircularLinkedList<T> extends CircularLinkedList<T> implements UnorderedListADT<T> {
-	
+	/**
+	 * Kostua konstantea O(1) esleipenak bakarrik egiten direlako eta esleipenen kostua kte. da
+	 */
 	public void addToFront(T elem) {
 	// hasieran gehitu
-		// KODEA OSATU ETA KOSTUA KALKULATU
-
+		Node<T> elemN = new Node<T>(elem);
+		elemN.next = last.next;
+		last.next = elemN;
 	}
-
+	/**
+	 * Kostua konstantea O(1) esleipenak bakarrik egiten direlako eta esleipenen kostua kte. da
+	 */
 	public void addToRear(T elem) {
 	// bukaeran gehitu
-		// KODEA OSATU ETA KOSTUA KALKULATU
-
+		Node<T> elemN = new Node<T>(elem);
+		elemN.next = last.next;
+		last.next = elemN;
+		last = elemN;
+		
 	}
-	
+	//target elementua listan dagoela suposatu behar da
 	public void addAfter(T elem, T target) {
-		// KODEA OSATU ETA KOSTUA KALKULATU (AUKERAZKOA)
+		Node<T> current = last;
+		boolean aurkitu = false;
+		
+		while(!aurkitu){
+			if(current.data.equals(target)){
+				aurkitu = true;
+			}else{
+				current = current.next;
+			}
+		}
+		Node<T> berria = new Node<T>(elem);
+		berria.next = current.next;
+		current.next = berria;
+		if(current == last){
+			last = berria;
+		}
+		
 	}
 
 }
