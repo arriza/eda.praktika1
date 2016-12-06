d13df3f73e77426752148bf7703946055537e64b
Leire Viseras <leireviseras@gmail.com>
2016-12-06 11:25:32 +0100

    d

--- praktika3.eda/src/praktika/graph/MainProbaGrafoa.java
+++ praktika3.eda/src/praktika/graph/MainProbaGrafoa.java
@@ -6,20 +6,20 @@ import praktika3.eda.Stopwatch;
 public class MainProbaGrafoa {
 
 	public static void main(String[] args) {
-		String nomF = "fitxategiak/listaHandiagoa.txt";
+		String nomF = "fitxategiak/geureFitx.txt";
 		Stopwatch timer = new Stopwatch();
 		PelikulaZerrenda.getPelikulaZerrenda().zerrendaKargatu(nomF);
 		GraphHash g = new GraphHash();
 		g.grafoaSortu(PelikulaZerrenda.getPelikulaZerrenda());
-		//g.print();
+		g.print();
 		System.out.println("Grafoa eraikia! :), emandako denbora --> "+timer.elapsedTime());
 		System.out.println("==============================================================");
 		
-		/*System.out.println("Erlazionatuta daude Casino eta A team ???? ");
+		System.out.println("Erlazionatuta daude Casino eta A team ???? ");
 		System.out.println(g.erlazionatuta("Casino", "A team"));
 		System.out.println("Erlazioa aurkitzeko emandako denbora --> "+timer.elapsedTime());
 		
-		System.out.println(g.erlazioBidea("Casino", "A team"));*/
+		//System.out.println(g.erlazioBidea("Casino", "A team"));
 		
 	}
 
